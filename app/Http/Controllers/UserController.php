dabe8c0b1563ad2447740b69b3ca935699e626c9
Jod Fedlet PIERRE <fedletpierre15@gmail.com>
2023-02-01 17:46:37 -0300

    Update UserController.php
    Signed-off-by: Jod Fedlet PIERRE <fedletpierre15@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,7 +70,6 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
         try {
             $response = $this->userAction->update($id, $request->all());
             return ResponseDto::success(ResponseDto::OK, $response);
